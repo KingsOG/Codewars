06d99c9c595bac413b29e72e0c0d70671a83a82a
Kingsley Ogbonna <kingsog@Kingsleys-MacBook-Air.local>
2022-04-16 22:57:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//convert a number to a string
+
+function numToString(num){
+    return numToString();
+}
+
+//Find the smallest integer in the array
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  }
+  
